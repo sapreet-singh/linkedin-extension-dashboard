9caf65e07e049df41a31e532c6b2dd849af5483f
Sapreet Singh <Sapreet.Singh@autviz.in>
2025-08-18 17:50:41 +0530

    Added a sidebar component for navigation with links to Dashboard and Profile. Created a Dashboard component with a simple template. Updated app routing to redirect to the Dashboard by default.

---
+++ src/sidebar/sidebar.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss'
+})
+export class SidebarComponent {
+ isCollapsed = true;
+
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+}
